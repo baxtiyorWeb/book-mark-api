399ad6b05899c68f10291aac484e47be139ff4a5
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-07-31 23:01:49 +0500

    fix #2

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,24 +1,24 @@
 import type { Document, Types } from "mongoose";
 
 export interface IUser extends Document {
-	_id: Types.ObjectId;
-	first_name: string;
-	last_name: string;
-	email: string;
-	password: string;
-	pass_salt?: string;
-	verification_token?: string;
-	valid_for?: Date;
-	isVerified?: boolean;
-	createdAt: Date;
-	updatedAt: Date;
+  _id: Types.ObjectId;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  pass_salt?: string;
+  verification_token?: string;
+  valid_for?: Date;
+  isVerified?: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface IReqUser {
-	_id: string;
-	first_name: string;
-	last_name: string;
-	email: string;
-	createdAt: Date;
-	updatedAt: Date;
+  _id: Types.ObjectId;
+  first_name: string;
+  last_name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
