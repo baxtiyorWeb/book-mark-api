28f88fdba427baf4f5f160974056fa5334d76c94
SomeOne1305 <axmadxolmuminov2007@gmail.com>
2025-07-31 18:58:55 +0500

    Vercel configuration #7

--- app/index.ts
+++ app/index.ts
@@ -6,8 +6,16 @@ import "dotenv/config";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT).on("listening", async () => {
-	await connectToDB();
-	logger.info(`Server is listening on port http://localhost:${PORT}`);
+  await connectToDB();
+  logger.info(`Server is listening on port http://localhost:${PORT}`);
 });
 
+export default function handler(req: any, res: any) {
+  return new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) return reject(err);
+      resolve(undefined);
+    });
+  });
+}
 module.exports = app;
